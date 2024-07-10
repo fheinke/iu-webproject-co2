17d75254befab7851814d6abfc4e982aa1a15bbf
Felix Heinke <felix@heinke.eu>
2024-07-10 12:45:35 +0200

    feat: initial layout with navbar and footer

---
+++ script.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("copyright").innerHTML = "&copy; " + new Date().getFullYear() + " Karl Felix Heinke";
+}, false);
