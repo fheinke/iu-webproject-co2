ff9c11e8daec9e81e917743cbd8aee0cfdb87d01
Felix Heinke <felix@heinke.eu>
2024-07-13 16:43:14 +0200

    feat: make colorTheme choice persistent with cookies

--- src/scripts/colorThemeToggler.js
+++ src/scripts/colorThemeToggler.js
@@ -1,9 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const getStoredTheme = localStorage.getItem('theme')
+
+    if (getStoredTheme === null || getStoredTheme === '') {
+        console.log('No theme selected');
+        localStorage.setItem('theme', 'light');
+    } else {
+        toggleColorTheme(getStoredTheme);
+    }
+}, false);
+
 document.getElementById('toggle-color-theme').addEventListener('click', () => {
-    if (document.documentElement.getAttribute('data-bs-theme') === 'dark') {
+    if (document.documentElement.getAttribute('data-bs-theme') === 'light') {
+        toggleColorTheme('dark')
+    } else {
+        toggleColorTheme('light')
+    }
+})
+
+function toggleColorTheme(theme) {
+    if (theme === 'light') {
         document.documentElement.setAttribute('data-bs-theme', 'light')
         document.getElementById('toggle-color-theme-image').setAttribute('class', 'bi bi-moon-stars-fill');
+        localStorage.setItem('theme', 'light');
     } else {
         document.documentElement.setAttribute('data-bs-theme', 'dark')
         document.getElementById('toggle-color-theme-image').setAttribute('class', 'bi bi-brightness-high-fill');
+        localStorage.setItem('theme', 'dark');
     }
-})
+}
